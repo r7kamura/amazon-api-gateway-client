c92ebc9153db7fd304d9d00aa305189e349c873a
Ryo Nakamura <r7kamura@gmail.com>
2015-08-02 01:06:50 +0900

    Fix indent

--- src/client.js
+++ src/client.js
@@ -5,60 +5,60 @@ import Restapi from './restapi'
 /**
  * @class Client
  */
- export class Client {
-   /**
-    * @param {String} accessKeyId
-    * @param {String} region
-    * @param {String} secretAccessKey
-    */
-   constructor({ accessKeyId, region, secretAccessKey }) {
-     this.accessKeyId = accessKeyId;
-     this.region = region;
-     this.secretAccessKey = secretAccessKey;
-   }
+export class Client {
+  /**
+   * @param {String} accessKeyId
+   * @param {String} region
+   * @param {String} secretAccessKey
+   */
+  constructor({ accessKeyId, region, secretAccessKey }) {
+    this.accessKeyId = accessKeyId;
+    this.region = region;
+    this.secretAccessKey = secretAccessKey;
+  }
 
-   /**
-    * @return {Fetcher}
-    */
-    getFetcher() {
-      if (!this._fetcher) {
-        this._fetcher = this._buildFetcher();
-      }
-      return this._fetcher;
+  /**
+   * @return {Fetcher}
+   */
+  getFetcher() {
+    if (!this._fetcher) {
+      this._fetcher = this._buildFetcher();
     }
+    return this._fetcher;
+  }
 
-   /**
-    * @return {Promise}
-    */
-   listRestapis() {
-     return this.getFetcher().get(`${this._getBaseUrl()}/restapis`)
-       .then((response) => {
-         return response.json();
-       }).then((source) => {
-         return source.item.map((element) => {
-           return new Restapi(element);
-         });
-       });
-   }
+  /**
+   * @return {Promise}
+   */
+  listRestapis() {
+    return this.getFetcher().get(`${this._getBaseUrl()}/restapis`)
+      .then((response) => {
+        return response.json();
+      }).then((source) => {
+        return source.item.map((element) => {
+          return new Restapi(element);
+        });
+      });
+  }
 
-   /**
-    * @return [Fetcher]
-    */
-   _buildFetcher() {
-     return new Fetcher().use(
-       AwsSignerV4,
-       {
-         accessKeyId: this.accessKeyId,
-         region: this.region,
-         secretAccessKey: this.secretAccessKey
-       }
-     );
-   }
+  /**
+   * @return [Fetcher]
+   */
+  _buildFetcher() {
+    return new Fetcher().use(
+      AwsSignerV4,
+      {
+        accessKeyId: this.accessKeyId,
+        region: this.region,
+        secretAccessKey: this.secretAccessKey
+      }
+    );
+  }
 
-   /**
-    * @return {String}
-    */
-   _getBaseUrl() {
-     return `https://apigateway.${this.region}.amazonaws.com`;
-   }
- }
+  /**
+   * @return {String}
+   */
+  _getBaseUrl() {
+    return `https://apigateway.${this.region}.amazonaws.com`;
+  }
+}
