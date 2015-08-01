b5d8d4b59c7e779fbbd890be33d9d192711b1c29
Ryo Nakamura <r7kamura@gmail.com>
2015-08-02 04:39:45 +0900

    Use RejectLogger middleware for debug

--- test/client.js
+++ test/client.js
@@ -1,13 +1,16 @@
 import { Client } from '../src/index.js'
-import { RequestLogger, ResponseLogger } from 'stackable-fetcher'
+import { RejectLogger, RequestLogger, ResponseLogger } from 'stackable-fetcher'
 
 describe('Client', () => {
   let client = new Client({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     region: process.env.AWS_REGION,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  })
-  client.getFetcher().use(RequestLogger).use(ResponseLogger);
+  });
+  client.getFetcher()
+    .use(RejectLogger)
+    .use(RequestLogger)
+    .use(ResponseLogger);
 
   describe('#createResource', () => {
     it('does not raise any error', (done) => {
