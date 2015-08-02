6122e67899dd529e986c90755ebe5058ae115011
Ryo Nakamura <r7kamura@gmail.com>
2015-08-02 12:01:12 +0900

    Use Mock middleware for testing

--- test/client-test.js
+++ test/client-test.js
@@ -1,15 +1,22 @@
 import { Client } from '../src/index.js'
-import { RequestLogger, ResponseLogger } from 'stackable-fetcher'
+import { Mock } from 'stackable-fetcher'
 
 describe('Client', () => {
-  let client = new Client({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    region: process.env.AWS_REGION,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  const client = new Client({
+    accessKeyId: 'accessKeyId',
+    region: 'region',
+    secretAccessKey: 'secretAccessKey',
   });
-  client.getFetcher()
-    .use(RequestLogger)
-    .use(ResponseLogger);
+
+  client.getFetcher().use(
+    Mock,
+    {
+      body: JSON.stringify({ item: [] }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  );
 
   describe('#createResource', () => {
     it('does not raise any error', (done) => {
@@ -58,7 +65,7 @@ describe('Client', () => {
     it('does not raise any error', (done) => {
       client.listResources({
         restapiId: 'restapiId'
-      }).catch((error) => {
+      }).then((error) => {
         done();
       });
     });
